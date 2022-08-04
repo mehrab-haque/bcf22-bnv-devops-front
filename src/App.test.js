6dfc61f33a20d77421427cb61a7ce2e4a05f7fc3
mehrab-haque <mehrab.haque.0001@gmail.com>
2022-08-04 06:30:23 +0600

    test suit

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,34 @@ import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import App from "./App";
+import Auth from "./components/admin/Auth";
+import Main from "./components/admin/Main";
+import Dashboard from "./components/admin/Dashboard";
 
-describe("App", () => {
+describe("Component App Render", () => {
     it("renders without crashing given the required props", () => {
         const wrapper = shallow(<App/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+describe("Component Main Render", () => {
+    it("renders without crashing given the required props", () => {
+        const wrapper = shallow(<Main/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+describe("Component Auth Render", () => {
+    it("renders without crashing given the required props", () => {
+        const wrapper = shallow(<Auth/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+describe("Component Dashboard Render", () => {
+    it("renders without crashing given the required props", () => {
+        const wrapper = shallow(<Dashboard/>);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
